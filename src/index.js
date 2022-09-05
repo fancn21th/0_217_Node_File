28ec44e303f3da5230526169f334df86d2f7ca58
fancn21th <fancn21th@gmail.com>
2022-09-05 11:47:19 +0800

    file stat

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 #!/usr/bin/env node
 
-console.log({ __dirname, cwd: process.cwd() });
+const fs = require("fs");
+const path = require("path");
+
+const fileName = "data.json";
+
+const filePath = path.join(process.cwd(), "./.data", fileName);
+
+fs.stat(filePath, (err, stats) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const status = {
+    isFile: stats.isFile(), // true
+    isDirectory: stats.isDirectory(), // false
+    isSymbolicLink: stats.isSymbolicLink(), // false
+    size: stats.size, // 1024000 //= 1MB
+  };
+
+  console.log({ status });
+});
